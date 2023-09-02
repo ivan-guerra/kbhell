7dee7bbde954bdd281d6ec0dcc90db4385b5cb25
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-09-02 00:10:44 -0700

    Have the Linux build script build docs and tests by default.
    This change makes the build.sh behavior on Linux most similar to the
    build.bat behavior on Windows. Temporarily, we've disabled the setup of
    the test directory in the top level CMakeLists.txt due to there being no
    unit tests just yet.

--- scripts/linux/build.sh
+++ scripts/linux/build.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
-BUILD_DOCS="OFF"
-BUILD_TESTS="OFF"
-BUILD_TYPE="Release"
-
 source config.sh
-
-Help()
-{
-    echo "build the kbhell project"
-    echo
-    echo "usage: build.sh [OPTION]..."
-    echo "options:"
-    echo -e "\t-g    enable debug info"
-    echo -e "\t-d    build project docs"
-    echo -e "\t-t    build unit tests"
-    echo -e "\t-h    print this help message"
-}
 
 Main()
 {
     # Create the build directory if it does not already exist.
     mkdir -pv $KBHELL_BUILD_DIR
 
+    # Set the build type according to the first program arg if any.
+    BUILD_TYPE="Release"
+    if [ ! -z  "$1" ]
+    then
+        BUILD_TYPE=$1
+    fi
+
     pushd $KBHELL_BUILD_DIR > /dev/null
         cmake ../ \
               -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-              -DBUILD_DOCS=$BUILD_DOCS \
-              -DPACKAGE_TESTS=$BUILD_TESTS \
+              -DBUILD_DOCS=ON \
+              -DPACKAGE_TESTS=ON \
               -DCMAKE_INSTALL_PREFIX=$KBHELL_BIN_DIR \
               -DCMAKE_BUILD_TYPE=$BUILD_TYPE && \
         make -j$(nproc) all                  && \
@@ -41,18 +32,4 @@ Main()
     popd > /dev/null
 }
 
-while getopts ":hgdt" flag
-do
-    case "$flag" in
-        g) BUILD_TYPE="Debug";;
-        d) BUILD_DOCS="ON";;
-        t) BUILD_TESTS="ON";;
-        h) Help
-           exit;;
-       \?) echo "error: invalid option '$OPTARG'"
-           Help
-           exit;;
-    esac
-done
-
-Main
+Main $1
